9d0fbfaf5c8b139caf2ef770893aaed510c59918
Chris VanGroll <chrisvangroll@gmail.com>
2021-09-21 12:34:53 -0500

    verify password plug in  works

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const bouncer = require('express-bouncer')(500, 900000);
+const verifyPassword = require('../middleware/verifyPassword')
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', verifyPassword, userCtrl.signup);
 router.post('/login', bouncer.block, userCtrl.login);
 
 module.exports = router;
