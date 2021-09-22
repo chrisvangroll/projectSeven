17d9951770de8715f9858412c18b465c9c5b7f62
Chris VanGroll <chrisvangroll@gmail.com>
2021-09-22 16:32:26 -0500

    Can send file on submit

--- frontend/src/makePost.js
+++ frontend/src/makePost.js
@@ -8,12 +8,17 @@ import Nav from './nav';
 function MakePost (props) {
 
     const [title, setTitle] = useState('');
-   // const [file, setFile] = useState('');
+    const [file, setFile] = useState();
 
     const sendTitle = (e) =>{
         setTitle(e.target.value);
         console.log(title)
     }
+    console.log(file)
+    // const sendFile = (selectedFile) =>{
+    //     setFile(selectedFile.target.files);
+    //     console.log(file)
+    // }
 
     // const sendFile = (file) =>{
     //     const formData = new FormData();
@@ -25,14 +30,15 @@ function MakePost (props) {
     //     }
     //     console.log(file[0])
     // }
-    const sendPost = async (file)=>{
+    const sendPost = async ()=>{
         // function getStorage(){
         //     let userId = localStorage.getItem('id');
         //     userId = JSON.parse(userId);
         //     return userId;
         // }
+        //console.log(file[0]);
         const formData = new FormData();
-        formData.append('image', file[0]);
+        formData.append('gif', file[0]);
         const config = {
             headers: {
                 'content-type': 'multipart/form-data'
@@ -54,7 +60,7 @@ function MakePost (props) {
                 <label htmlFor="title">Title</label>
                 <input type="text" id="title" onChange = {sendTitle}/>
                 <label htmlFor="file">Upload your file</label>
-                <input type="file" id="file" onChange = {(e)=> {sendPost(e.target.files)}}/>
+                <input type="file" id="file" onChange = {(e) => setFile(e.target.files)}/>
             </form>
             <br />
             <button onClick = {sendPost}>Create Post</button>
