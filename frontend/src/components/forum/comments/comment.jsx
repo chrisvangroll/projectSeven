fd61161c105726f87a497dbf447114cab2ea3569
Chris VanGroll <chrisvangroll@gmail.com>
2021-09-28 14:00:39 -0500

    edit comments done

--- frontend/src/components/forum/comments/comment.jsx
+++ frontend/src/components/forum/comments/comment.jsx
@@ -7,10 +7,14 @@ function Comment (props) {
 
     
     const [commentEdit, setCommentEdit] = useState('');
+    const [commentEditInput, setCommentEditInput] = useState('');
 
     const logCommentEdit = (e) =>{
         setCommentEdit(e.target.value);
+        setCommentEditInput(e.target.value);
     }
+
+    //console.log(commentEdit)
 
     function getStorage(){
         let userId = localStorage.getItem('id');
@@ -53,18 +57,19 @@ const finalToggle = () =>{
     // console.log(getStorage())
     //console.log(props.commenter)
     return(
-        <div class= 'd-flex flex-row flex-wrap'>
+        
+        <div class= 'd-flex flex-row flex-wrap position-relative'>
             <div class= 'w-75'>{props.comment}</div>
             <div class='w-25 d-flex flex-row'>
                 <CommentLikes commentId2 = {props.commentId}/>
                 <div className={toEdit()} onClick = {toggleClass} ><i class="fas fa-edit"></i></div>
             </div>
-            <input type="text" onChange={logCommentEdit} id={`editBox${props.commentId}`}  class='position-absolute border-5 d-none'/>
+            <input type="text" onChange={logCommentEdit} id={`editBox${props.commentId}`} placeholder={props.comment} class='position-absolute border-5 d-none'/>
             <div id={`editSubmit${props.commentId}`} class='d-none'>
                 <button onClick={updateComment} >Submit Edit</button>
                 <button onClick={finalToggle}>cancel</button>
             </div>
-            <div id={`edit${props.commentId}`} class='d-none'>
+            <div id={`edit${props.commentId}`} class='d-none position-absolute end-0 top-100 deleteAndEditContainer'>
                 <DeleteComment getComments1={props.getComments} commentId4={props.commentId}/>
                 <button onClick={toggleEditBox}>update</button>
             </div>
