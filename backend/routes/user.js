3e0f5b15bd3166e33b68b62cf03820998fcf8c2b
Chris VanGroll <chrisvangroll@gmail.com>
2021-10-04 12:07:58 -0500

    Installed bouncer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,8 +5,8 @@ const bouncer = require('express-bouncer')(500, 900000);
 const verifyPassword = require('../middleware/verifyPassword')
 
 router.post('/signup', verifyPassword, userCtrl.signup);
-// router.post('/login', bouncer.block, userCtrl.login);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block, userCtrl.login);
+//router.post('/login', userCtrl.login);
 router.delete('/delete', userCtrl.deleteUser )
 
 module.exports = router;
